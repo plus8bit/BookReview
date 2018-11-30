18a97d366159569012377f56516a71418c9e9d37
Nick <plus0bit@gmail.com>
2018-11-30 23:27:23 +0300

    Implement books show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,12 @@
 class BooksController < ApplicationController
+
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -21,6 +27,10 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :description, :author)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
   end
 
 
